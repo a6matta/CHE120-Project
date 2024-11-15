a9225c27cbf79f5e691ba0a3ab7e177f2623090a
a38hasan <a38hasan@uwaterloo.ca>
2024-11-15 16:49:52 -0500

    Update snake_with_comments.py

--- snake_with_comments.py
+++ snake_with_comments.py
@@ -51,11 +51,17 @@ def move():
     #represents the snake moving forward (since the list reflects the snake's body from head to tail - the consistent updatng
     #of head position makes the snake appear to be moving forward as required in game play) 
     
-    #AH -> # This section is to check if snake eats the food 
-    if head == food:
+    #AH -> # This section is to check if snake "eats" the food and then update the game accordingly 
+    if head == food: #AH -> The condition if head == food, is to check if the snake's head position matches the food position (meaning it can "eat" the food)
         print('Snake:', len(snake)) #AH -> #Print length of snake
+        #AH -> If the if head == food condition is true and the snake can eat the food, the program will print the current length of the snake
+        #AH -> Then, the following section updates the food position to a new random location on the grid: 
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
+        #AH -> The above is accomplished by assigning random x and y coordinate values to the "food" using randrange()
+        #randrange(start,stop,step) generates random numbers from a specified range, the range specified will generate a random integer between -15 and 15 that will 
+        #represent possible positions in terms of the game grid (the x10 is potentially to scale the grid positions to actual coordinates for the food to be placed?)
+        #the above ensures that the food will appear somwhere else on the screen for the snake to chase and the game play to continue
     else:
         snake.pop(0)
 
