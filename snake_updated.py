8dfbd991a1c4a644c346675f81eaf61950694a49
a38hasan <a38hasan@uwaterloo.ca>
2024-11-18 11:51:19 -0500

    Update snake_updated.py

--- snake_updated.py
+++ snake_updated.py
@@ -7,49 +7,54 @@ Exercises
 3. How would you move the food?
 4. Change the snake to respond to mouse clicks.
 """
-
+#Import Statements remain the same as the original game 
 from random import randrange
 from turtle import *
 
 from freegames import square, vector
 
 food = vector(0, 0)
-snake = [vector(10, 0)]
-aim = vector(0, -10)
 
+#The snake and the snake's direction should also remain the same
+snake = [vector(10, 0)] 
+aim = vector(0, -10) 
 
+#Doesn't need to be altered 
 def change(x, y):
     """Change snake direction."""
     aim.x = x
     aim.y = y
 
-
+#Doesn't need to be altered 
 def inside(head):
     """Return True if head inside boundaries."""
     return -200 < head.x < 190 and -200 < head.y < 190
 
-
+#Doesn't need to be altered 
 def move():
     """Move snake forward one segment."""
     head = snake[-1].copy()
     head.move(aim)
 
+#________________________________________________________________________________________________________
+
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
         update()
         return
-
-    snake.append(head)
+    
+    snake.append(head) #Makes the snake appear to be moving so also does not need to be changed 
 
     if head == food:
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
-        snake.pop(0)
+        snake.pop(0) #Does the altered game require the snake to grow size? Or do we maintain the same size? If so, this is still needed 
+        #But probably need to move it up since we'll need to change the if statement
 
-    clear()
-
+    clear() #Ensures smooth transitions, also stays the same 
+    #Buildng of snake does not need to be altered 
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
@@ -57,7 +62,7 @@ def move():
     update()
     ontimer(move, 100)
 
-
+#Setup stays the same 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
