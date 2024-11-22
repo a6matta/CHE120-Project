c312398452e6c33bf4ed219b137e89ce24eb5ea2
a38hasan <a38hasan@uwaterloo.ca>
2024-11-21 23:49:26 -0500

    Update snake_updated.py

--- snake_updated.py
+++ snake_updated.py
@@ -22,6 +22,8 @@ screen boundaries will end the game
 
 Things to consider:
 - I don't think we consider the condition of the obstacles/food/snake overlapping with each other? We might want to avoid that as well
+- Also AM suggested we could increase the range more to make the game harder. I don't know if that will be necessary but we could 
+always have it as an option to up the difficulty if needed. 
 """
 from random import randrange  # Import the randrange function for generating random positions
 from turtle import *  # Import all turtle functions for graphics and animation
